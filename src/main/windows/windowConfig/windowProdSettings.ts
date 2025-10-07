100c7d64c63f56e474a2be572f08fbf2cb2fd081
Ankit Sharma <ankit.sharma@filuet.com>
2025-10-07 11:38:21 +0530

    scaling .08

--- src/main/windows/windowConfig/windowProdSettings.ts
+++ src/main/windows/windowConfig/windowProdSettings.ts
@@ -2,7 +2,7 @@ import { BrowserWindowConstructorOptions, app } from 'electron';
 import { resolve } from 'path';
 
 // Set Chromium command-line switches before app is ready
-app.commandLine.appendSwitch('force-device-scale-factor', '1.02'); // Force 100% scaling regardless of system setting
+app.commandLine.appendSwitch('force-device-scale-factor', '1.08'); // Force 100% scaling regardless of system setting
 app.commandLine.appendSwitch('high-dpi-support', '1'); // Enable high-DPI support without scaling
 
 export const windowProdSettings: BrowserWindowConstructorOptions = {
