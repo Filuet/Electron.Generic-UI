5e83a75d8e137ad9f3ea4526ac5bd6997cf17e88
Ankit Sharma <ankit.sharma@filuet.com>
2025-10-13 19:14:23 +0530

    test: updating the new version

--- src/renderer/src/Temp.tsx
+++ src/renderer/src/Temp.tsx
@@ -1,7 +1,9 @@
 import { JSX } from 'react';
 
 function Temp(): JSX.Element {
-  return <h1>it automatically installed the changes </h1>;
+  return (
+    <h1>I have done some new changes please verify that it updated in the installed app or not </h1>
+  );
 }
 
 export default Temp;
