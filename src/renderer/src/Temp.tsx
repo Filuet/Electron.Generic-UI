49e8a65d4edf350d68aa966bbb1a8cfe52807c51
Ankit Sharma <ankit.sharma@filuet.com>
2025-11-03 09:59:20 +0530

    refactor: removed temporary test component

--- src/renderer/src/Temp.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from 'react';
-
-function Temp(): JSX.Element {
-  return <div>this is test </div>;
-}
-export default Temp;
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import ThemeCustomization from './theme';
 import store from './redux/rootStore';
-// import App from './App';
+import App from './App';
 import './main.css';
 import './localization';
-import Temp from './Temp';
 
 const rootElement = document.getElementById('root');
 
@@ -13,8 +12,7 @@ if (rootElement) {
   createRoot(rootElement).render(
     <ThemeCustomization>
       <Provider store={store}>
-        {/* <App /> */}
-        <Temp />
+        <App />
       </Provider>
     </ThemeCustomization>
   );
